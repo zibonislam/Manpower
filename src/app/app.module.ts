43523d3143fd57c3c5bafa35d02d5045a791b941
zibonislam <105701288+zibonislam@users.noreply.github.com>
2023-01-12 13:11:19 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './MyCom/login/login.component';
+import { HomeComponent } from './MyCom/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
