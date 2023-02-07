7b24e210c23e2f212a55ef596a300ddac926e05e
zibonislam <jibon133002@gmail.com>
2023-02-08 03:09:44 +0600

    8/2/2023

--- src/app/MyCom/registration/registration.component.ts
+++ src/app/MyCom/registration/registration.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/MyService/auth.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+
+  form: any = {
+    username: null,
+    // nid: null,
+    email:null,
+    password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+
+
+
+  
+  constructor(private authService: AuthService) { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+
+    this.authService.register(username, email, password).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    });
+  }
 
 }
